eba1c9acd010b4a15bacc4032ea61ebf54a67d05
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-11-07 05:05:09 +0000

    automatically backed up by learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
 
   def create
     comment = Comment.create(comment_params)
-    post = Post.find(comment_params[:user_id])
+    post = Post.find_by(comment_params[:user_id])
     post.comments << @comment
     redirect_to @comment.post
   end
